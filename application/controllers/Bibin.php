13c7cdb2ada7ae66650c313659adcc1fd2fe3bf8
Slawek <yellowpostman@gmail.com>
2019-09-17 11:22:38 +0300

    initial state

---
+++ application/controllers/Bibin.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bibin extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        redirect('https://www.linkedin.com/in/s%C5%82awek-bezborodov/','location');
+    }
+}
